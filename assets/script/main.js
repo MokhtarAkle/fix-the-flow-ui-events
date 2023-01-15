dd6a35de69913a1214dded2d1e9fa88ad0fd1cb5
MokhtarAkle <mokhtar.akle@hva.nl>
2023-01-15 21:37:12 +0100

    progress

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,22 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+let interaction = document.querySelector('a:nth-of-type(12)');
+let spin = document.querySelector("a:nth-of-type(1)");
+let hop = document.querySelector("a:nth-of-type(3)");
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
+spin.addEventListener("click", spinHandler);
+hop.addEventListener("keyup", hopHandler);
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
+}
+
+function spinHandler(){
+  spin.classList.toggle("spin");
+}
+
+function hopHandler(event){
+  if(event.keyCode === 32){
+    hop.classList.toggle("hop");
+  }
 }
